403dc2253f73ab2caca7c2f9dbbfdd9e97e5baa4
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2023-03-29 11:36:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import axios  from 'axios';
 
-function App() {
+
+function DemoApp() {
+  const [events, setEvents] = useState([]);
+
+
+  const fetchEvents = async () => {
+    try {
+      const config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/employes/attendance',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await axios.request(config);
+      setEvents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event');
+    let isPresent = prompt('Is the student present? (yes/no)') === 'yes';
+
+    if (title) {
+      axios
+        .post('http://localhost:3000/employes/attendance', {
+          studentId: title,
+          present: isPresent,
+          date: selectInfo.startStr,
+        })
+        .then((response) => {
+          console.log(response,"response")
+          fetchEvents()
+        
+          console.log('Attendance recorded successfully');
+        })
+        .catch((error) => {
+          console.error('Error recording attendance', error);
+        });
+    }
+  };
+
+      useEffect(()=>{
+        fetchEvents()
+      },[])
+
+    const transformedEvents =   events?.map((event) => ({
+      title: event.studentId,
+      date: new Date(event.date).toISOString().substring(0, 10),
+      backgroundColor: event.present ? 'green' : 'red',
+    }));
+
+    console.log(transformedEvents)
+
+
+
+  // const eventsDATA = [
+  //   { title: "event 1", date: "2023-03-07" ,backgroundColor:'green'},
+  //   { title: "event 2", date: "2023-03-17" }
+  // ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-app">
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+       
+          dayMaxEvents={true}
+          events={transformedEvents}
+          select={handleDateSelect}
+        
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default DemoApp;
